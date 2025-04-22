4e6ef933f753e322e35ade76e76a08c773bc9ea0
Kushal Rathod <168859142+KushalXCoder@users.noreply.github.com>
2025-04-22 15:10:08 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/U-Well/',
-})
+  build: {
+    target: 'esnext',  // This ensures modern JavaScript is used
+  },
+});
